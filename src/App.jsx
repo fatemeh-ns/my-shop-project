8fbd246976ea355e0a56b1b7924a12d41516004b
fatemeh-on <fomidi491@gmail.com>
2025-04-29 11:30:47 +0430

    update

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Footer from "./components/Footer";
 import Search from "./components/Search";
 import Cart from "./pages/Cart";
 import ProductDetail from "./pages/ProductDetail";
+import NotFound from "./pages/NotFound";
 
 export default function App() {
   return (
@@ -17,6 +18,7 @@ export default function App() {
         <Route path="/cart" element={<Cart />} />
         <Route path="/search" element={<Search />} />
         <Route path="/product/:id" element={<ProductDetail />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
       <Footer />
     </BrowserRouter>
---
+++ src/pages/NotFound/index.jsx
@@ -0,0 +1,9 @@
+const NotFound = () => {
+  return (
+    <div className= "h-[100vh] text-center mt-20 text-2xl font-medium">
+      <p>صفحه مورد نظر پیدا نشد! (404)</p>
+    </div>
+  );
+};
+
+export default NotFound;
